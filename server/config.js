2edec92095e7c523365bce8c8bd90addf697ccae
Rodrigo Cisternas <rodcisal@gmail.com>
2014-02-16 01:06:19 -0300

    added github login

---
+++ server/config.js
@@ -0,0 +1,9 @@
+Accounts.loginServiceConfiguration.remove({
+  service: "github"
+});
+
+Accounts.loginServiceConfiguration.insert({
+  service: "github",
+  clientId: "f156295feee6d975bc2c",
+  secret: "63cdc0612a474d89af3faaf2d7a8ec59e7b32811"
+})
